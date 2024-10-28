87e9bc2478024911d10ee3d0d279d11826a0845e
向鹏程 <404481864@qq.com>
2024-10-28 12:08:11 +0800

    1

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -6,7 +6,7 @@ document.querySelector('.login-button').addEventListener('click', function () {
 // Validate password and handle login
 function validatePassword() {
     var password = document.getElementById('passwordInput').value;
-    if (password === '123456') {
+    if (password === '123123') {
         alert('登录成功！');
         window.location.href = 'AIXIN/爱心代码.html';
     } else {
